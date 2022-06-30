ba10489473630a6122f6afb637e46b065baf7463
han-gyeong <hangyeong@outlook.com>
2022-06-30 22:51:13 +0900

    Initial Project

---
+++ src/main/java/com/triple/travelermileage/TravelerMileageApplication.java
@@ -0,0 +1,13 @@
+package com.triple.travelermileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelerMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelerMileageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/triple/travelermileage/TravelerMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.travelermileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerMileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
