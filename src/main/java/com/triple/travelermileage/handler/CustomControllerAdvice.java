dee561397d20fd98c900aa589e8653a759043dd1
han-gyeong <hangyeong@outlook.com>
2022-07-01 21:29:29 +0900

    feat: 잘못된 TYPE 이나 ACTION 입력값 예외처리 추가

--- src/main/java/com/triple/travelermileage/handler/CustomControllerAdvice.java
+++ src/main/java/com/triple/travelermileage/handler/CustomControllerAdvice.java
@@ -2,6 +2,7 @@ package com.triple.travelermileage.handler;
 
 import com.triple.travelermileage.response.Message;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +12,10 @@ public class CustomControllerAdvice {
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<Message> argumentExceptionHandler(IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(new Message(exception.getMessage(), null));
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<Message> notReadableException() {
+        return ResponseEntity.badRequest().body(new Message("TYPE이나 ACTION 이 잘못 입력되었습니다.", null));
     }
 }
