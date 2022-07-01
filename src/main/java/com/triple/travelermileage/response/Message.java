d378744e17038824da8dfb22383ca606f086cff0
han-gyeong <hangyeong@outlook.com>
2022-07-01 21:00:31 +0900

    edit: Message 에 static 생성자 추가

--- src/main/java/com/triple/travelermileage/response/Message.java
+++ src/main/java/com/triple/travelermileage/response/Message.java
@@ -11,4 +11,8 @@ public class Message {
         this.message = message;
         this.content = content;
     }
+
+    public static Message ok(Object content) {
+        return new Message("정상처리 되었습니다.", content);
+    }
 }
